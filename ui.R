6ed47868df0352d322b4248a6d40ee53cee7c2e1
dsnoor <alacrity.n@gmail.com>
2015-04-28 20:12:31 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,16 +3,31 @@ library(shiny)
 
 shinyUI(fluidPage(
 
-    titlePanel("Data Science Capstone: Word Prediction Model using NLP"),
+    titlePanel("Word Prediction Model using NLP"),
     
     # Copy the line below to make a text input box
     
     hr(),
-    # Text input box
-    textInput("text", label = h4("Enter your text here to predict next word..."), value = "Good"),
-    
-    
-    h4('Next predicted word(s): ', style = "color:blue;"),
-    fluidRow(column(3, verbatimTextOutput("value")))
 
+    h4('Message: ', style = "color:blue;"),
+    fluidRow(column(3, verbatimTextOutput("message"), style="color:red")),
+    
+    # Text input box
+    h4('Enter your text below: ', style = "color:blue;"),
+    fluidRow(column(3, textInput("text", label ="", value = "Good"))),
+    
+    h4('Predicted next word(s) are:', style = "color:blue;"),
+    fluidRow(column(3, verbatimTextOutput("value"))),
+    h6('Note: first word is of high probability than the subsequent words shown above. '),
+    hr(),
+    
+    h4('Sample phrases from corpus(for testing):', style ="color:grey"),
+    h5('Example #1: Hey sunshine, can you follow me and make me the happiest', style = "color:grey"),
+    h5('Example #2: Be grateful for the good times and keep the faith', style = "color:grey"),
+    h5('Example #3: Every inch of you is perfect from the bottom to the top', style = "color:grey"),
+    h5('Example #4: Id give anything to see arctic monkeys', style = "color:grey"),
+    h5('Example #5: It would mean the world', style = "color:grey"),
+    h5('Example #6: Good morning everyone.', style = "color:grey"),
+    
+    hr()
 ))
